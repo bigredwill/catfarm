dfb1bc68b07f666527bd05bbda92dc6ea80e3cac
Henry Tran <hen@Henrys-MBP.leia.com>
2015-12-04 22:05:03 -0800

    car depth

--- src/scene.js
+++ src/scene.js
@@ -29,8 +29,8 @@ function createScene(scene) {
 
 		window.car = object;
 
-		object.scale.set(1, 1, 1)
-		object.position.z = -20;
+		object.scale.set(1, 3, 1)
+		object.position.z = -50;
 		object.position.y = 0;
 
 		// object.rotation.z = Math.PI;
