955ca2eae61aac1b83c41e6b0e2345c0f838f529
Henry Tran <hen@Henrys-MBP.leia.com>
2015-12-04 20:03:32 -0800

    Added leia code and fixed resolution

--- gruntfile.js
+++ gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
 
     watch: {
       scripts: {
-        files: ['**/*.js', '*.html', 'index.html'],
+        files: ['**/*.js', '*.html', 'index.html', '*'],
         tasks: [],
         options: {
           livereload: true,
