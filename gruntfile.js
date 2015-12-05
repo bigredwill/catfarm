9701633ed9268d16963f57ca1d2c969f7bfb0174
Henry Tran <hen@Henrys-MBP.leia.com>
2015-12-04 19:39:24 -0800

    grunt watch stuff

---
+++ gruntfile.js
@@ -0,0 +1,47 @@
+
+module.exports = function(grunt) {
+
+  // project configuration 
+  grunt.initConfig({
+
+    watch: {
+      scripts: {
+        files: ['**/*.js', '*.html', 'index.html'],
+        tasks: [],
+        options: {
+          livereload: true,
+          atBegin: true,
+          spawn: true
+        },
+      },
+    },
+
+
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          keepalive: false,
+          livereload: true,
+          base: {
+            path: '.',
+            options: {
+              index: 'index.html',
+              maxAge: 300000
+            }
+          }
+        }
+      }
+    }
+
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+
+  grunt.registerTask('default', ['connect', 'watch']);
+
+}
+
