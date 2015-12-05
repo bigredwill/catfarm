099e9c9e5474439aa5c76aedbc004aea9372b1ab
Henry Tran <hen@Henrys-MBP.leia.com>
2015-12-04 21:20:05 -0800

    added 3d care

--- src/scene.js
+++ src/scene.js
@@ -6,7 +6,44 @@ function createScene(scene) {
     var greenMaterial = new THREE.MeshBasicMaterial( { color: 0xff00ff } );
     var cube = new THREE.Mesh(boxGeometry, greenMaterial);
 
+	var ambient = new THREE.AmbientLight( 0x444444 );
+	scene.add( ambient );
+
+	var directionalLight = new THREE.DirectionalLight( 0xffeedd );
+	directionalLight.position.set( 0, 0, 1 ).normalize();
+	scene.add( directionalLight );
+
     // Add The Cube To The Scene
-    scene.add(cube);
+    // scene.add(cube);
+
+/*****/
+
+	setInterval(function() {
+		cube.rotation.x += 0.01;
+		cube.rotation.y += 0.05;
+	}, 0)
+
+
+	var loader = new THREE.OBJMTLLoader();
+	loader.load( './models/mustang impala.obj', './models/mustang impala.mtl', function ( object ) {
+
+		window.car = object;
+
+		object.scale.set(1, 1, 1)
+		object.position.y = -5;
+
+		setInterval(function() {
+			object.rotation.x += 0.01;
+			object.rotation.y += 0.05;
+		}, 100);
+
+		object.rotation.x += 10;
+		var bbox = new THREE.Box3().setFromObject(object);
+		console.log(bbox);
+
+		scene.add( object );
+
+	} );
+
 
 }
