569c6ab9d19b80f2157d3376e0834cb0a4a652d1
Henry Tran <hen@Henrys-MacBook-Pro.local>
2015-12-05 02:03:38 -0800

    fake camera moving scene around

--- src/scene.js
+++ src/scene.js
@@ -3,7 +3,7 @@ var OBJMTLLoader = new THREE.OBJMTLLoader();
 var TextureLoader = new THREE.TextureLoader();
 
 
-function createScene(scene) {
+function createScene(scene, camera, looper) {
 
 	var car;
 
@@ -19,7 +19,7 @@ function createScene(scene) {
 		/* Load the car model! */
 		OBJMTLLoader.load( './models/mustang impala.obj', './models/mustang impala.mtl', function ( object ) {
 
-			object.scale.set(0.5, 1, 0.5)
+			object.scale.set(0.25, 1, 0.25)
 			object.position.z = -0.1;
 			object.position.y = 13;
 			object.position.x = 13;
@@ -50,7 +50,7 @@ function createScene(scene) {
 					 } );
 
 					var road = new THREE.Mesh(
-						new THREE.PlaneGeometry(20, 20),
+						new THREE.PlaneGeometry(100, 100),
 						material
 					);
 					road.position.z = 0;
@@ -80,6 +80,67 @@ function createScene(scene) {
 	// 	cube.rotation.x += 0.01;
 	// 	cube.rotation.y += 0.05;
 	// }, 0)
+
+	var keysDown = {}
+
+	window.addEventListener("keydown", function(event) {
+		keysDown[String.fromCharCode(event.keyCode)] = true;
+	});
+
+	window.addEventListener("keyup", function(event) {
+		keysDown[String.fromCharCode(event.keyCode)] = false;
+	});
+
+	var time = (new Date()).getTime() / 1000;
+	var oldTime = time;
+	var delta = 0;
+	looper(function() {
+		oldTime = time;
+		time = (new Date()).getTime() / 1000;
+		var delta = time - oldTime;
+
+		if (keysDown["T"]) {
+			car.position.x += 30 * delta * Math.cos(car.rotation.y + Math.PI/2);
+			car.position.y += 30 * delta * Math.sin(car.rotation.y + Math.PI/2);
+				
+			if (keysDown["F"]) {
+				car.rotation.y += 2 * delta;
+			}
+				
+			if (keysDown["H"]) {
+				car.rotation.y -= 2 * delta;
+			}
+		}
+
+		if (keysDown["G"]) {
+			car.position.x -= 30 * delta * Math.cos(car.rotation.y + Math.PI/2);
+			car.position.y -= 30 * delta * Math.sin(car.rotation.y + Math.PI/2);
+				
+			if (keysDown["F"]) {
+				car.rotation.y -= 2 * delta;
+			}
+				
+			if (keysDown["H"]) {
+				car.rotation.y += 2 * delta;
+			}
+		}
+
+		var camPos = new THREE.Vector2(camera.position.x, camera.position.y);
+		camPos.lerp(new THREE.Vector2(car.position.x, car.position.y), 0.25);
+
+		var camRot = new THREE.Quaternion();
+		var camTo = new THREE.Quaternion();
+		var camFinal = new THREE.Euler();
+		camRot.setFromEuler(new THREE.Euler(camera.rotation.z, 0, 0));
+		camTo.setFromEuler(new THREE.Euler(car.rotation.y, 0, 0));
+		camRot.slerp(camTo, 0.08);
+		camFinal.setFromQuaternion(camRot);
+
+		camera.position.x = camPos.x;
+		camera.position.y = camPos.y;
+		camera.rotation.z = camFinal.x;
+
+	});
 
 
 
